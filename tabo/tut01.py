c727ec601eda3d7c9c0f8353cce63ba083edeae4
yorchek <yorchek@localhost.localdomain>
2014-11-20 00:16:34 -0600

    tarjeta y promocions

--- tabo/tut01.py
+++
@@ -1,9 +0,0 @@
-import cherrypy
-
-class HelloWorld(object):
-    @cherrypy.expose
-    def index(self):
-        return "Hello world!"
-
-if __name__ == '__main__':
-   cherrypy.quickstart(HelloWorld())
