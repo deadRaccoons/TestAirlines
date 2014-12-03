dc4ac55083e7ca269e3e982bcbd397621a494927
Jonathan Andrade <ndrd@ciencias.unam.mx>
2014-12-03 00:08:40 -0600

    barra laterial con login overlay

--- mamelines/app/controllers/welcome_controller.rb
+++ mamelines/app/controllers/welcome_controller.rb
@@ -1,4 +1,9 @@
 class WelcomeController < ApplicationController
   def index
+  	@usuario = nil
+  	if (session[:current_user_id] != nil)
+  		 @usuario = Usuario.find(session[:current_user_id])
+  	end
+
   end
 end
