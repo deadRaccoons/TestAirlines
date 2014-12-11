587824f3c26f27f4448b4db9f3225edd8745d41c
Jonathan Andrade <ndrd@ciencias.unam.mx>
2014-12-10 18:56:00 -0600

    yield

--- site/public/assets/js/controls.js
+++ site/public/assets/js/controls.js
@@ -1,3 +1,5 @@
+  var receive_sugesstions = true; 
+
  $(function() {
 
   $logo = $("#ll");
@@ -11,18 +13,74 @@
   });
   
   var destino = $("#destiny-query").val();
-  var sugesstions = "?q=";
 
-  $("#origin-query").keyup(function(e){
-      $.getJSON( "/ciudads", {
-            q: $(this).val(),
-          })
-         .done(function( data ) {
-            $data = $("#origin-suggestions");
-            show_destiny_suggestions(data, $data);
-            $data.show('slow');
-          });
+
+  $("#origin-query").keypress(function(e) {
+      if (e.keyCode == 8) {
+          receive_sugesstions =  true;
+        }
+
+      if (receive_sugesstions) {
+        $.getJSON( "/ciudads", {
+              q: $(this).val(),
+            })
+           .done(function( data ) {
+              $data = $("#origin-suggestions");
+              show_destiny_suggestions(data, $data);
+              $data.show('slow');
+            });
+      } else {
+        $("#origin-suggestions").hide();
+      } 
+      
+
     });
+
+
+   $("#origin-suggestions").delegate(".ss", "click", function(){
+        var name = $(this).attr("data-city_name");
+
+      $(this).hide();
+      receive_sugesstions =  false;
+      $("#origin-query").val(name);
+
+   })
+
+   $("#destiny-query").focus(function() {
+      $("#ll4").html($logo);
+      $( ".logo-head" ).hide('slow');
+      $("#fly-details").show('slow');
+  });
+
+   var rs = true;
+
+   $("#destiny-query").keypress(function(e) {
+    if (e.keyCode == 8) {
+        rs =  true;
+      }
+      if (rs) {
+        $.getJSON( "/ciudads", {
+              q: $(this).val(),
+            })
+           .done(function( data ) {
+              $data = $("#destiny-suggestions");
+              show_destiny_suggestions(data, $data);
+              $data.show('slow');
+            });
+      } else {
+        $("#destiny-suggestions").hide();
+      } 
+
+    });
+
+   $("#destiny-suggestions").delegate(".ss", "click", function() {
+      var name = $(this).attr("data-city_name");
+      rs =  true;
+      $(this).hide();
+      $("#destiny-query").val(name);
+
+   })
+
   
 
 });
@@ -32,7 +90,7 @@ var show_destiny_suggestions =  function(json, container) {
   container.empty();
   var html  = "<ul>";
     $.each(json, function( index, ciudad ) {
-       html += "<li><a data='hola'><i>"+ ciudad.IATA +"</i><o>" + ciudad.nombre + "</o></a><li>"
+       html += "<li><a class='ss' data-city_name='"+ "[" + ciudad.IATA+"]" +  " " + ciudad.nombre  +"'><i>"+ ciudad.IATA +"</i><o>" + ciudad.nombre + "</o></a><li>"
 
     });
     html  += "</ul>";
