cd2e4a9c75dc3a3554b8c1330663bb1d5fc6f0ed
Rackso <oscarvilla55@hotmail.com>
2014-12-16 21:41:11 -0600

    agregue una funcion que puede que necesitemos para que facebook sirva

--- site/facebookjs.js
+++ site/facebookjs.js
@@ -1,12 +1,22 @@
 <body>
 <div id="fb-root"></div>
-<script>(function(d, s, id) {
+<script>
+ /window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '1440584586186104',
+      xfbml      : true,
+      version    : 'v2.2'
+    });
+  };/
+
+(function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
   js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
   fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
+}(document, 'script', 'facebook-jssdk'));
+</script>
 </body>
 
 
