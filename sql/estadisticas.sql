d0c05bb1268e99a4e1a78a30249048e013fb5f95
yorchek <kuberjorg3n@hotmail.com>
2014-12-31 18:41:39 -0600

    ultimo

---
+++ sql/estadisticas.sql
@@ -0,0 +1,7 @@
+﻿select fechasalida, count(*) from viajes group by fechasalida order by fechasalida
+
+--para avion
+update avion set disponible = 'n' where idavion = 1
+update avion set disponible = 'y' where idavion = 1
+select sum(distancia) from viajes where idavion = 1 and realizado = 'y'
+select destino from viajes where idavion = 1 order by fechallegada desc limit 1
